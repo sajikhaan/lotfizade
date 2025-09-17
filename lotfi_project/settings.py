6be8bd5363b1c9942e070e95fd8f56a4a5e88afb
sajikhaan <sjadmfakhry6@gmail.com>
2025-09-17 07:19:27 -0700

    Set STATIC_ROOT and collect static files

--- lotfi_project/settings.py
+++ lotfi_project/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-temp-key'
@@ -70,7 +70,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
