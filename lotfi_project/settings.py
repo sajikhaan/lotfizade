3aa897eca356f79948e03262cfaa21348edfbe19
sajikhaan <sjadmfakhry6@gmail.com>
2025-09-17 06:19:16 -0700

    Fix allowed hosts for Render

--- lotfi_project/settings.py
+++ lotfi_project/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-temp-key'
 DEBUG = False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["lotfizade.onrender.com", "127.0.0.1", "localhost"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
